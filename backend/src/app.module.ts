dab4b33a8c55acb856e854bb7a6496e21d9c1439
TMN <tommy.martin@adsoftware.fr>
2025-06-17 13:09:38 -0500

    feat: init

---
+++ backend/src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { getDatabaseConfig } from './config/database.config';
+import { MoviesModule } from './movies/movies.module';
+import { ActorsModule } from './actors/actors.module';
+import { RatingsModule } from './ratings/ratings.module';
+import { SeedingModule } from './seeding/seeding.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => getDatabaseConfig(configService),
+    }),
+    MoviesModule,
+    ActorsModule,
+    RatingsModule,
+    SeedingModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
